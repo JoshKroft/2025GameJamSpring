df870905e1e9cbbe3eeb0710236358a3bfd57b82
YouieDel <littlepostage@gmail.com>
2025-02-09 10:45:59 -0600

    Lets try

--- cast_shadow/cast_shadow.gd
+++ cast_shadow/cast_shadow.gd
@@ -1,11 +1,4 @@
-extends Node
+class_name CastShadow extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
