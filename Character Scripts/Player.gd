8ea9c60e31e96a6f65459c1dfeda54c389ee436b
YouieDel <littlepostage@gmail.com>
2025-02-12 22:21:18 -0600

    AlmostDone

--- Character Scripts/Player.gd
+++ Character Scripts/Player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var animation = $Sprite/AnimationPlayer
+
 @export var move_speed : float = 100
 var hearts_list : Array[TextureRect]
 var health = 3
@@ -18,10 +20,24 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-
+	
+	if Input.is_action_pressed("left"):
+		$Sprite.flip_h = true
+		animation.play("Walk")
+	elif Input.is_action_pressed("right"):
+		$Sprite.flip_h = false
+		animation.play("Walk")
+	elif Input.is_action_pressed("up"):
+		animation.play("Walk")
+	elif Input.is_action_pressed("down"):
+		animation.play("Walk")
+	else:
+		animation.play("Idle")
+		
 	#input velocity 
 	velocity = input_direction * move_speed 
 	move_and_slide()
+	
 	
 func take_damage():
 	if $InvulnerabilityTimer.is_stopped():	
@@ -46,14 +62,6 @@ func update_heart_display():
 	# player dead
 	if health <= 0:
 		alive = false
-		game_over()
 	
 func game_over():
-	if alive == false:
-		get_tree().change_scene_to_file("res://Menus/GameOverScreen.tscn")
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://Menus/WinMenu.tscn")
-	
+	get_tree().reload_current_scene()
--- cast_shadow/cast_shadow.gd
+++
@@ -1,4 +0,0 @@
-class_name CastShadow extends Node
-
-@onready var ray_cast_2d: RayCast2D = $RayCast2D
-@onready var sprite_2d: Sprite2D = $Sprite2D
